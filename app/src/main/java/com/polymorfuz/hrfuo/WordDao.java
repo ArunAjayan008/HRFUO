80a8f7a847d54b1c38227e93ea4385ee9f9b3823
Arun Ajayan <arun.frdcrwz@gmail.com>
2020-04-18 01:39:43 +0530

    Version 1.0

--- app/src/main/java/com/polymorfuz/hrfuo/WordDao.java
+++ app/src/main/java/com/polymorfuz/hrfuo/WordDao.java
@@ -1,12 +1,13 @@
 package com.polymorfuz.hrfuo;
 
 import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
-
+@Dao
 public interface WordDao {
     // allowing the insert of the same word multiple times by passing a
     // conflict resolution strategy
